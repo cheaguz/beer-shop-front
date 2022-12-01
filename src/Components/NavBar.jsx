83df6e1b55c6cac52fe14a53bae2dec90b252bde
Cheaguz <aguzf83@gmail.com>
2022-12-01 19:40:19 -0300

    nav , navbar created

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,31 +1,71 @@
 import React from 'react';
-import { Box,Link,  } from "@chakra-ui/react";
+import { Box, Link, Input, Button, Text } from "@chakra-ui/react";
 import { useNavigate } from 'react-router-dom';
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillBoxPlot, AiOutlineSearch } from "react-icons/ai";
+
 
 export const NavBar = () => {
   const navigate = useNavigate();
- 
+
   return (
-    
-        <Box
-         width={'100%'}
-         boxShadow='dark-lg'
-         bg={'orange.200'}
-         display='flex'
-         justifyContent={'space-between'}
-         
-         >
-            <span>The birras Shop</span>  
-        
-              <input type={'text'}/>
-              <div>
-              <Link onClick={()=> navigate('/contact',{replace:true}) }>Contacto</Link>
-              <Link onClick={()=> navigate('/',{replace:true}) }>Home</Link>
-              
-              </div>
-              
-           
-        </Box>
-    
+
+    <Box
+      width={'100%'}
+      boxShadow='dark-lg'
+      bg={'#D8C378'}
+      h={42}
+      display='flex'
+      justifyContent={'space-between'}
+      alignContent='center'
+      textAlign={'center'}
+    >
+      <Text
+        fontFamily={'Jolly Lodger'}
+        color='white'
+        fontSize={23}
+        ml={2}
+        textAlign='center'
+      >
+        <Link pr={2} color='white' onClick={() => navigate('/', { replace: true })}>The birras Shop</Link>
+      </Text>
+
+      <Box
+        display={'flex'}
+        alignItems='center'
+      >
+        <Input
+          w={'90%'}
+          h='70%'
+          color='white'
+          bg='white'
+          type={'text'}
+        />
+        <Button
+          h='70%'
+          bg={'blackAlpha.100'}
+          variant='ghost'
+          p={0}
+        >
+          <AiOutlineSearch />
+        </Button>
+
+      </Box>
+
+      <Box
+        display={'flex'}
+        justifyContent='center'
+        alignContent={'center'}
+        alignItems='center'
+        textAlign={'center'}
+      >
+        <Link pr={2} color='white' onClick={() => navigate('/contact', { replace: true })}>Login</Link>
+        <Link pr={2} color='white' onClick={() => navigate('/', { replace: true })}>
+          < FaShoppingCart />
+        </Link>
+      </Box>
+
+    </Box>
+
   )
 }
